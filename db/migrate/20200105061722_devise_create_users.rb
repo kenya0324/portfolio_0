fbd8031656116a4a56f1c410487eab8d6065fd4a
Kenya <kenya0324@gmail.com>
2020-01-27 06:45:34 +0000

    userカラムの修正

--- db/migrate/20200105061722_devise_create_users.rb
+++ db/migrate/20200105061722_devise_create_users.rb
@@ -42,7 +42,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :deleted_at
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
