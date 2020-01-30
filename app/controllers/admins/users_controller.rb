b2b92ad856a36e3e12a4b1bd1efa0f7b05c8ae1e
Kenya <kenya0324@gmail.com>
2020-01-30 08:26:41 +0000

    admin側機能修正

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -2,12 +2,20 @@ class Admins::UsersController < ApplicationController
 	before_action :set_category, only: [:index, :category, :hashtag, :search, :show,]
 
 	def index
-        @users = User.all.order(created_at: :desc)
+      @users = User.all.order(created_at: :desc)
 	end
 
 	def show
-  	    @user = User.find(params[:id])
-    end
+  	  @user = User.find(params[:id])
+  end
+
+  def hide
+      @user = User.find(params[:id])
+      @user.update(is_deleted: true)
+      session.delete(:user)
+      flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしてお待ちしております"
+      redirect_to admins_posts_path
+  end
 
   private
 
